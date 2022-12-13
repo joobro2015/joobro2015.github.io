31d0e55b1c5e723788603ee79247ae3f258a8f05
ict05-09 <ict05-09@DESKTOP-GGJ2RO6>
2022-12-13 17:21:09 +0900

    practice

---
+++ _posts/morningtest.java
@@ -0,0 +1,45 @@
+package prj;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class morningtest {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		int count = 0;
+		int max = -1;
+		int index = -1;
+
+		FileInputStream fis = new FileInputStream("res\\data.txt");
+
+		Scanner scan = new Scanner(fis); // fis의 값을 읽어오겠다.
+
+		String line = scan.nextLine(); // 메모장 내용을 전부 들고오기
+//		line = "20 30 29 39 49 38 10 19 87 29 38 27 8 90 87 "
+
+		String[] tokens = line.split(" "); // " " 공백을 기준으로 토큰에 넣어주기
+		
+		
+		
+		// line ["20 30 29 39 49 38 10 19 87 29 38 27 8 90 87 "]
+//		max = Integer.parseInt(tokens[0]);
+
+//		for(int i = 0; i < tokens.length; i++) 
+//		}
+//			if (Integer.parseInt(tokens[i]) == 10)
+//				index = i;
+				
+//				System.out.printf("index : %d 입니다.", index);
+//		}
+//		System.out.printf("max : %d", max);
+
+		while(scan.hasNext()) {
+			scan.next();
+			count++;
+		System.out.println(count);
+		}
+	}// main
+
+}// class
---
+++ _posts/omoktest3.java
@@ -0,0 +1,48 @@
+package prj;
+
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class omoktest3 {
+	public static void main(String[] args) {
+
+//		----------------------------------------------------------------
+
+		int ox, oy;
+
+		System.out.println("oy, ox를 입력해 주세요");
+		System.out.print("(y x)>");
+		Scanner scan = new Scanner(System.in);
+		oy = scan.nextInt();
+		ox = scan.nextInt();
+
+		System.out.printf("oy : %d,ox : %d\n", oy, ox);
+
+		char[][] board = new char[10][10];
+//		char[] board = new char[100];
+	
+		for (int y = 1; y <= 10; y++)
+			for (int x = 1; x <= 10; x++)
+				board[y - 1][x - 1] = '┼';
+
+		for (int i = 0; i < 10; i++)
+			board[0][i] = '┬';
+		
+		
+		board[oy - 1][ox - 1] = '○';
+//		board[oy - 1][ox - 1] = '●';
+
+		for (int y = 1; y <= 10; y++) {
+			for (int x = 1; x <= 10; x++)
+				System.out.printf("%c", board[y - 1][x - 1]);
+
+			System.out.println();
+			}
+		}
+		// 사용자로부터 다음 모양의 입력을 요청한다.
+		// (x,y) > 3 4 -> nextInt(), nextInt()
+		
+	}// main
+
+}
+// class
